0e061f37506eef4f72f636a3bae9e9eef7f5cb2c
Manish Tekam <manish.tekam.9@gmail.com>
2023-05-15 15:36:46 +0530

    Product moel added

--- store/models.py
+++ store/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from category.models import Category
 
 # Create your models here.
+
+# model for storing product info
+class Product(models.Model):
+    product_name = models.CharField(max_length=200, unique=True)
+    description  = models.TextField(max_length=500, blank=True)
+    price        = models.IntegerField()
+    slug         = models.SlugField(max_length=200, unique=True)
+    stock        = models.IntegerField()
+    images       = models.ImageField(upload_to='photos/products', height_field=None, width_field=None, max_length=None)
+    is_available = models.BooleanField(default=True)
+    # foreign key for category model
+    category     = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product_name
+
+
+
