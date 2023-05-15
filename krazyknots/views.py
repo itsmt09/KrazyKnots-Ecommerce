73746a49eec8a9e36f55723d5db770c0290e2346
Manish Tekam <manish.tekam.9@gmail.com>
2023-05-15 16:28:50 +0530

    Configuring context for index.html

--- krazyknots/views.py
+++ krazyknots/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from store.models import Product
 
 def home(request):
+    #Passing only available products in context
+    products = Product.objects.all().filter(is_available = True)
+    context = {
+        'products': products,
+    }
     return render(request, 'index.html')
